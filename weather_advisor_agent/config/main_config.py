20165650233a671948ff5c84cb8687aa69f3ea21
Jorge C <jorge.a.conde.j@gmail.com>
2026-06-04 22:01:40 -0600

    Changes on new Gemini models

--- weather_advisor_agent/config/main_config.py
+++ weather_advisor_agent/config/main_config.py
@@ -13,10 +13,10 @@ logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(leveln
 
 @dataclass
 class TheophrastusConfiguration:
-  worker_model: str = "gemini-2.5-flash"
-  critic_model: str = "gemini-2.5-pro"
-  mapper_model: str = "gemini-2.0-flash-lite"
-  writer_model: str = "gemini-2.0-flash-lite"
+  worker_model: str = "gemini-3.1-flash-lite"
+  critic_model: str = "gemini-3.5-flash"
+  mapper_model: str = "gemini-2.5-flash-lite"
+  writer_model: str = "gemini-2.5-flash-lite"
   max_iterations: int = 2
   
   model_params: dict = field(default_factory=lambda: {
